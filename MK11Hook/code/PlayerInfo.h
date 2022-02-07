0b886966e35309ef907bd8fa18e6d8c79b4b2e22
ermaccer <40604575+ermaccer@users.noreply.github.com>
2022-02-07 21:24:54 +0100

    small edit

--- MK11Hook/code/PlayerInfo.h
+++ MK11Hook/code/PlayerInfo.h
@@ -3,9 +3,9 @@ enum eMeterValues {
 	BAR_Offensive,
 	BAR_Defensive
 };
+
 class PlayerInfo {
 public:
 	void SetMeter(eMeterValues type, float value);
 	void SetDamageMult(float value);
 };
-//1405D2B30
