a76290dca00a22eee78fcb625bc53e1cf87dfb8f
ermaccer <40604575+ermaccer@users.noreply.github.com>
2022-02-26 09:22:52 +0100

    cleanup

--- MK11Hook/code/PlayerInfo.cpp
+++ MK11Hook/code/PlayerInfo.cpp
@@ -18,24 +18,6 @@ void PlayerInfo::SetDamageMult(float value)
 
 void PlayerInfo::SetColor(FName color, FVector* value)
 {
-	//PlayerInfo* info = (PlayerInfo*)((int64)this - 1056);
-	if (GetGameInfo())
-	{
-		PlayerInfo* p1 = GetInfo(PLAYER1);
-		PlayerInfo* p2 = GetInfo(PLAYER2);
-		if (p1 || p2)
-		{
-			printf("THIS - %x P1: %x P2: %x\n", this, (int64)GetInfo(PLAYER1) + 1056, (int64)GetInfo(PLAYER2) + 1056);
-		}
-	}
-
-
-	//character_info* chr = *(character_info**)((int64)this - 1056 + 216);
-	//if (chr)
-	//	printf("Blood for %s\n", chr->name);
-
-
-	*value = { 0.5,0.5,0 };
 	((void(__fastcall*)(PlayerInfo*, FName, FVector*))_addr(0x140E4E240))(this, color, value);
 }
 
